78fe4d52287df47b6c835808a4531c11a273c939
Dronicsam <110901407+Dronicsam@users.noreply.github.com>
2024-04-07 22:35:40 +0300

    :art: Site structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
-import type { PropsWithChildren } from "react";
+import "@mantine/core/styles.css";
 
-export default function RootLayout({ children }: PropsWithChildren) {
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import type { ReactNode } from "react";
+
+export const metadata = {
+  title: "My Mantine app",
+  description: "I have followed setup instructions carefully",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <ColorSchemeScript />
+        <title></title>
+      </head>
       <body>
-        <div>
-          <div className="root">{children}</div>
-        </div>
+        <MantineProvider>{children}</MantineProvider>
       </body>
     </html>
   );
---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,5 @@
+export const Home = () => (
+    <div>
+        123
+    </div>
+)
